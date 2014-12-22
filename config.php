ea9d597968e28fdba679a99426b30c40eba21edd
nookls <admin@nookls.org>
2014-12-22 14:00:37 +0000

    Update config.php
    oops forgot to show the options

--- config.php
+++ config.php
@@ -9,5 +9,5 @@ $unchi['password']  = ""; // sha512 hash of password
 $unchi['upld_dir']  = "src/"; // Upload dir for images, files, etc. Should have a / at the end.
 $unchi['thumb_dir'] = "src/thumb/"; // Location for storing thumbnails.
 $unchi['mem_limit'] = "1000M"; // Temporary memory limit while uploading. I recommend making it ridiculously high just in case.
-$unchi['def_view']  = "gallery"; // Default view to use in the gallery. Defaults to gallery (the image thumbnail mode)
+$unchi['def_view']  = "gallery"; // (gallery/list) Default view to use in the gallery. Defaults to gallery (the image thumbnail mode)
 $unchi['pg_amount'] = 100; // Amount of files to show per page (applies to both views)
